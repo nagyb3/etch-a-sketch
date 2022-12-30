a43f5081bc07babd6174f4a42d279c1fa4f1f2cc
nagyb3 <nagybence2003@gmail.com>
2022-12-30 22:54:07 +0100

    button added and button correctlys resizes cells

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
 
 
 
@@ -13,12 +13,57 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(row);
 }
 
-const allCellList = document.querySelectorAll('.cell');
+let allCellList = document.querySelectorAll('.cell');
 
 for (let i = 0; i < allCellList.length; i++) {
     allCellList[i].addEventListener('mouseenter', () => {
         allCellList[i].style.backgroundColor = "blue";
     });
 }
+
+//total width and height is 800px
+
+
+const removeChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
+
+
+const alertButton = document.querySelector('.alert-button');
+alertButton.addEventListener('click', () => {
+    let sketchPadSize = parseInt(prompt("Enter a number for the size of the sketch pad! (MAX 64)"));
+    if (sketchPadSize > 64) {
+        sketchPadSize = 64;
+    }
+
+    removeChilds(container);
+
+    let cellHeightWidth = 800 / sketchPadSize;
+
+    for (let i = 0; i < sketchPadSize; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        for (let i = 0; i < sketchPadSize; i++) {
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.style.width = `${cellHeightWidth}px`;
+            cell.style.height = `${cellHeightWidth}px`;
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+
+    console.log(container);
+    let allCellList = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < allCellList.length; i++) {
+        allCellList[i].addEventListener('mouseenter', () => {
+            allCellList[i].style.backgroundColor = "blue";
+        });
+    }
+})
+
 
 console.log(allCellList);
